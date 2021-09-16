2769294edf46e19a41185d970538e60a3efdb5a5
Jakub Dolejší <xdolejsi@evo17.mendelu.cz>
2021-09-16 07:36:49 +0200

    ä

---
+++ OpenCV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by Jakub Dolejší on 16/09/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
